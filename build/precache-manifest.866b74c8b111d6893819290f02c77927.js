002a7ab15bcf2bf3e3ac5666f6bfcb28e4d64f03
revygabor <revy.gabor@gmail.com>
2020-04-10 00:44:01 +0200

    add build

---
+++ build/precache-manifest.866b74c8b111d6893819290f02c77927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7300a9a907ccd4303bb84c4eab374e8d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b23b370acec1ef38dc5",
+    "url": "./static/css/main.5ffe11f5.chunk.css"
+  },
+  {
+    "revision": "894daed5cac13c915747",
+    "url": "./static/js/2.ee16ff66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ee16ff66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b23b370acec1ef38dc5",
+    "url": "./static/js/main.639af595.chunk.js"
+  },
+  {
+    "revision": "821e3e9e8623f25acdce",
+    "url": "./static/js/runtime-main.62724d6a.js"
+  }
+]);
